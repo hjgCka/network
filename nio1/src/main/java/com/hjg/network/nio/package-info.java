38c61b07bcbdb70676c9b2e0708811987075aed8
hjg <computer_cka@163.com>
2021-03-03 17:00:30 +0800

    添加注释。

---
+++ nio1/src/main/java/com/hjg/network/nio/package-info.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * nio是一种同步非阻塞的I/O模型。
+ * nio是在等待就绪阶段是非阻塞的，避免了空等，浪费CPU时间。
+ * 真正进行I/O操作时，还是阻塞的。
+ *
+ * @description:
+ * @author: hjg
+ * @createdOn: 2021/3/3
+ */
+package com.hjg.network.nio;
